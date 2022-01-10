943049f20dac13b53ec930c4ebe2a98e085cf17c
Vinícius Schnoor <contato@viniciussantos.adm.br>
2022-01-10 19:51:31 -0300

    Modal Overlay Activate

--- scripts.js
+++ scripts.js
@@ -1,7 +1,10 @@
-Modal = {
+const Modal = {
     open(){
         // Abrir modal
+        const modalOverlay = document.querySelector('.modal-overlay')
+        // console.log(modalOverlay) --> Apenas para consultar div
         // Adicionar classe active ao modal
+        modalOverlay.setAttribute('class', 'modal-overlay active')
     },
     close(){
         // Fechar modal
