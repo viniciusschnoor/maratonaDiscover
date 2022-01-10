879383aee7b1c15a9ff9481aa4b2004579bc6ace
Vinícius Schnoor <contato@viniciussantos.adm.br>
2022-01-10 19:27:09 -0300

    Finished Modal

---
+++ scripts.js
@@ -0,0 +1,10 @@
+Modal = {
+    open(){
+        // Abrir modal
+        // Adicionar classe active ao modal
+    },
+    close(){
+        // Fechar modal
+        // Remover classe active do modal
+    }
+}
