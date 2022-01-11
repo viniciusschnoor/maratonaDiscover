ce66f2510e2dedd9e52d63970e6f613188070e17
Vinícius Schnoor <contato@viniciussantos.adm.br>
2022-01-10 21:42:49 -0300

    Update Performance

--- scripts.js
+++ scripts.js
@@ -1,13 +1,17 @@
 const Modal = {
     open(){
-        // Abrir modal
-        const modalOverlay = document.querySelector('.modal-overlay')
-        // console.log(modalOverlay) --> Apenas para consultar div
-        // Adicionar classe active ao modal
-        modalOverlay.setAttribute('class', 'modal-overlay active')
+        // // Abrir modal
+        // const modalOverlay = document.querySelector('.modal-overlay')
+        // // console.log(modalOverlay) --> Apenas para consultar div
+        // // Adicionar classe active ao modal
+        // modalOverlay.classList.add('active')
+        document.querySelector('.modal-overlay').classList.add('active')
     },
     close(){
-        // Fechar modal
-        // Remover classe active do modal
+        // // Fechar modal
+        // const modalOverlay = document.querySelector('.modal-overlay')
+        // // Remover classe active do modal
+        // modalOverlay.classList.remove('active')
+        document.querySelector('.modal-overlay').classList.remove('active')
     },
 }
